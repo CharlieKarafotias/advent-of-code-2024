81d5f5d25905a84cffa9244a21b4719b4a9f60ac
Charles Karafotias <cnkara2023@gmail.com>
2024-12-01 09:42:39 -0500

    updating gitignore

--- day1/target/debug/day1.d
+++
@@ -1,1 +0,0 @@
-/Users/charlie/code/advent-of-code-2024/day1/target/debug/day1: /Users/charlie/code/advent-of-code-2024/day1/src/main.rs
--- day1/target/debug/deps/day1-482c7106f650a2cf.d
+++
@@ -1,5 +0,0 @@
-/Users/charlie/code/advent-of-code-2024/day1/target/debug/deps/day1-482c7106f650a2cf: src/main.rs
-
-/Users/charlie/code/advent-of-code-2024/day1/target/debug/deps/day1-482c7106f650a2cf.d: src/main.rs
-
-src/main.rs:
