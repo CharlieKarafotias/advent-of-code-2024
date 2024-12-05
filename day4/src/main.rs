d8cedcb63d3cc6d46537923a941256eaef40b072
Charles Karafotias <cnkara2023@gmail.com>
2024-12-05 12:18:16 -0500

    refactored day 4 to support part 2

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -3,15 +3,23 @@ fn main() {
     let file_path = std::path::Path::new("day_4_input.txt");
     let file = std::fs::read_to_string(file_path).expect("Unable to read file");
     let lines: Vec<&str> = file.lines().collect::<Vec<&str>>();
-    let crossword: Vec<Vec<char>> = lines.iter().map(|x| x.chars().collect()).collect(); // Vec<Vec<char>>
+    let crossword: Vec<Vec<char>> = lines.iter().map(|x| x.chars().collect()).collect();
     println!("Part 1 answer: {}", part_1(crossword.clone()));
 }
 
 fn part_1(crossword: Vec<Vec<char>>) -> u32 {
     let mut horizontal: Vec<String> = get_horizontal(crossword.clone());
     let mut vertical: Vec<String> = get_vertical(crossword.clone());
-    let mut diagonal_up_right: Vec<String> = get_diagonal(crossword.clone(), true);
-    let mut diagonal_down_right: Vec<String> = get_diagonal(crossword.clone(), false);
+    let mut diagonal_up_right =
+        get_diagonal(crossword.clone(), true)
+            .iter()
+            .map(|x| x.to_string())
+            .collect();
+    let mut diagonal_down_right =
+        get_diagonal(crossword.clone(), false)
+            .iter()
+            .map(|x| x.to_string())
+            .collect();
 
     let mut all_orientations_of_crossword: Vec<String> = Vec::new();
     all_orientations_of_crossword.append(&mut horizontal);
@@ -20,6 +28,37 @@ fn part_1(crossword: Vec<Vec<char>>) -> u32 {
     all_orientations_of_crossword.append(&mut diagonal_down_right);
 
     find_words(all_orientations_of_crossword, vec!["XMAS".to_string()])
+}
+
+fn part_2(_crossword: Vec<Vec<char>>) -> u32 {
+    // only use diagonal searches now
+    // need to find all mas/sam words - along with index of a
+    // if you find mas/sam words - and index of a is the same in both diagonal search, then add to sum
+    todo!()
+}
+
+struct Row {
+    row: Vec<(char, usize, usize)>,
+}
+impl Row {
+    fn new() -> Row {
+        Row { row: Vec::new() }
+    }
+    fn add_letter(&mut self, letter: char, row: usize, col: usize) {
+        self.row.push((letter, row, col));
+    }
+}
+
+impl std::fmt::Display for Row {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self
+            .row
+            .iter()
+            .map(|x| x.0)
+            .collect::<Vec<char>>()
+            .iter()
+            .collect::<String>())
+    }
 }
 
 fn get_horizontal(crossword: Vec<Vec<char>>) -> Vec<String> {
@@ -41,8 +80,8 @@ fn get_vertical(crossword: Vec<Vec<char>>) -> Vec<String> {
     result
 }
 
-fn get_diagonal(crossword: Vec<Vec<char>>, direction_up_right: bool) -> Vec<String> {
-    let mut result: Vec<String> = Vec::new();
+fn get_diagonal(crossword: Vec<Vec<char>>, direction_up_right: bool) -> Vec<Row> {
+    let mut result: Vec<Row> = Vec::new();
     let total_diagonals = crossword.len() + crossword[0].len() - 1;
     match direction_up_right {
         true => {
@@ -50,19 +89,19 @@ fn get_diagonal(crossword: Vec<Vec<char>>, direction_up_right: bool) -> Vec<Stri
             let mut c_row: usize = 0;
             let mut c_col: usize = 0;
 
-            for diagonal in 0..total_diagonals {
-                let mut diagonal_str: String = String::new();
+            for _diagonal in 0..total_diagonals {
+                let mut row = Row::new();
                 let mut r: usize = c_row;
                 let mut c: usize = c_col;
                 loop {
-                    diagonal_str.push(crossword[r][c]);
+                    row.add_letter(crossword[r][c], r, c);
                     if r == 0 || c == crossword[0].len() - 1 {
                         break;
                     }
                     r -= 1;
                     c += 1;
                 }
-                result.push(diagonal_str);
+                result.push(row);
                 if c_row < crossword.len() - 1 {
                     c_row += 1;
                 } else {
@@ -76,19 +115,19 @@ fn get_diagonal(crossword: Vec<Vec<char>>, direction_up_right: bool) -> Vec<Stri
             let mut c_row: usize = crossword.len() - 1;
             let mut c_col: usize = 0;
 
-            for diagonal in 0..total_diagonals {
-                let mut diagonal_str: String = String::new();
+            for _diagonal in 0..total_diagonals {
+                let mut row = Row::new();
                 let mut r: usize = c_row;
                 let mut c: usize = c_col;
                 loop {
-                    diagonal_str.push(crossword[r][c]);
+                    row.add_letter(crossword[r][c], r, c);
                     if r == crossword.len() - 1 || c == crossword[0].len() - 1 {
                         break;
                     }
                     r += 1;
                     c += 1;
                 }
-                result.push(diagonal_str);
+                result.push(row);
                 if c_row > 0 {
                     c_row -= 1;
                 } else {
@@ -141,7 +180,8 @@ mod tests {
             vec!['g', 'h', 'i']
         ];
         let expected = vec!["g".to_string(), "dh".to_string(), "aei".to_string(), "bf".to_string(), "c".to_string()];
-        assert_eq!(get_diagonal(input, false), expected);
+        let result: Vec<String> = get_diagonal(input, false).iter().map(|x| x.to_string()).collect();
+        assert_eq!(result, expected);
     }
 
     #[test]
@@ -152,7 +192,8 @@ mod tests {
             vec!['g', 'h', 'i']
         ];
         let expected = vec!["a".to_string(), "db".to_string(), "gec".to_string(), "hf".to_string(), "i".to_string()];
-        assert_eq!(get_diagonal(input, true), expected);
+        let result: Vec<String> = get_diagonal(input, true).iter().map(|x| x.to_string()).collect();
+        assert_eq!(result, expected);
     }
 
     #[test]
